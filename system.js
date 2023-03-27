78d6c3ff64b907b72403532369c50ae22dfc29a0
LordRyanII <ryanoliveiram2020@outlook.com>
2023-03-27 00:35:08 -0300

    Criando primeira parte da calculadora

--- system.js
+++ system.js
@@ -1,5 +1,27 @@
 //Start:
+/*
+    Tipo é paramêtros genericos que representam dois valores 'acao' e 'valor'
+*/
 
-function calcular(){
-    
-}
+
+function calcular(tipo, valor){
+    if(tipo === 'acao') {
+      if(valor === 'c'){
+        //limpar o visor
+        document.getElementById('resultado').value = ''
+      }
+      //Adiciona as operações matemáticas no visor
+      if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
+        document.getElementById('resultado').value += valor
+      }
+      //Soma os inputs
+      if(valor === '=') {
+        let valor_campo = eval(document.getElementById('resultado').value)
+        document.getElementById('resultado').value = valor_campo
+      }
+    //Exibe o Resultado
+    } else if (tipo === 'valor') {
+
+      document.getElementById('resultado').value += valor
+    }
+  }
