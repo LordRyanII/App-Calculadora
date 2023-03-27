b6222775339d44a992e57a4358557adbe4be3393
LordRyanII <ryanoliveiram2020@outlook.com>
2023-03-27 18:38:46 -0300

    FInalizando o app Calculadora

--- system.js
+++ system.js
@@ -3,25 +3,21 @@
     Tipo é paramêtros genericos que representam dois valores 'acao' e 'valor'
 */
 
-
 function calcular(tipo, valor){
-    if(tipo === 'acao') {
-      if(valor === 'c'){
-        //limpar o visor
-        document.getElementById('resultado').value = ''
-      }
-      //Adiciona as operações matemáticas no visor
-      if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
-        document.getElementById('resultado').value += valor
-      }
-      //Soma os inputs
-      if(valor === '=') {
-        let valor_campo = eval(document.getElementById('resultado').value)
-        document.getElementById('resultado').value = valor_campo
-      }
-    //Exibe o Resultado
-    } else if (tipo === 'valor') {
-
-      document.getElementById('resultado').value += valor
+  if(tipo === 'acao') {
+    if(valor === 'c'){
+      //limpar o visor
+      document.querySelector('#resultado').value = ''
     }
+    if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
+      document.querySelector('#resultado').value += valor
+    }
+    if(valor === '=') {
+      var valor_campo = eval(document.getElementById('resultado').value)
+      document.querySelector('#resultado').value = valor_campo
+    }
+  } else if (tipo === 'valor') {
+
+    document.querySelector('#resultado').value += valor
   }
+}
